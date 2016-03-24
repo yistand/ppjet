585ba4783bf848553213a2921131d77613fb4ca6
yistand <yistand@gmail.com>
2016-03-24 14:58:10 -0400

    small modification to loop over all input files

--- loop2submit.sh
+++ loop2submit.sh
@@ -16,7 +16,7 @@ for jetcharge in FullJet ChargeJet
 do
 	for undercharge in TransCharged TransNeutral  
 	do 
-		for i in 0 #{0..9}
+		for i in {0..9}
 		do
 			#bsub -q week -W 168:00 -M 120000 -R "rusage[mem=120000]" -J "${match}${jetcharge}${undercharge}_${trg}_${i}" "sh  submitjob.sh ${i} ${trg} ${jetcharge} ${undercharge} ${match}"		# MB
 			bsub -q shared -W 20:00 -M 50000 -R "rusage[mem=50000]" -J "${tag}${match}${jetcharge}${undercharge}_${trg}_${i}" "sh  submitjob.sh ${i} ${trg} ${jetcharge} ${undercharge} ${match} ${tag}"	# JP2
